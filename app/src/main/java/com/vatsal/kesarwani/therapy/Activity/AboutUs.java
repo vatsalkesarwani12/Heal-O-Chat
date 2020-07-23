a8c2e9432a3ae9ea679180ab32ac32732d6e3824
iamvatsal <vatsalkesarwani12@gmail.com>
2020-07-23 06:07:28 +0530

    about us done

--- app/src/main/java/com/vatsal/kesarwani/therapy/Activity/AboutUs.java
+++ app/src/main/java/com/vatsal/kesarwani/therapy/Activity/AboutUs.java
@@ -2,15 +2,58 @@ package com.vatsal.kesarwani.therapy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.vatsal.kesarwani.therapy.R;
 
 public class AboutUs extends AppCompatActivity {
+    private ImageButton linkedin,github,twitter;
+    private String llinkedin,lgit,ltweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        init();
+        llinkedin="https://www.linkedin.com/in/vatsal-kesarwani-4a3858171/";
+        lgit="https://github.com/plazzy99";
+        ltweet="https://twitter.com/KesarwaniVatsal";
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(llinkedin));
+                startActivity(i);
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(lgit));
+                startActivity(i);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(ltweet));
+                startActivity(i);
+            }
+        });
+
+    }
+    private  void init(){
+        linkedin=findViewById(R.id.linkedin);
+        github=findViewById(R.id.github);
+        twitter=findViewById(R.id.twitter);
     }
 }
